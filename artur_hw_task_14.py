a4a6c711d1aa2d32fcb58fcf59264d31bed5eee2
ArturDumanskyi <dumanskyi1991@gmail.com>
2018-03-06 12:13:53 +0200

    Task_14

---
+++ artur_hw_task_14.py
@@ -0,0 +1,23 @@
+# Задание №14
+# Написать функцию, которая будет проверять четность некоторого числа
+
+import math
+
+parity = input("Введите число: ")
+parity = int(parity)
+
+if parity % 2 == 0:
+    print("число парное")
+else:
+    print("число непарное")
+
+
+# def parity(number):
+#     return
+#     number = input("Введите число: ")
+#     number = int(number)
+#     if number % 2 == 0:
+#         print("число парное")
+#     else:
+#         print("число непарное")
+
