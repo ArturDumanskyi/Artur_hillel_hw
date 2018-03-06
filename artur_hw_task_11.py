c25c231e852e4e936119ede5ce1ffa79876992c5
ArturDumanskyi <dumanskyi1991@gmail.com>
2018-03-06 12:11:51 +0200

    Task_11

---
+++ artur_hw_task_11.py
@@ -0,0 +1,14 @@
+
+
+
+import math
+
+def degrees2radians(a):
+    return a / 180 * math.pi
+
+print()
+print("косинус угла в 40 градусов равен:", round(math.cos(degrees2radians(40)), 5))
+print()
+print("косинус угла в 45 градусов равен:", round(math.cos(degrees2radians(45)), 5))
+print()
+print("косинус угла в 60 градусов равен:", round(math.cos(degrees2radians(60)), 5))
