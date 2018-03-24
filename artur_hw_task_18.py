803f4129bb211c442ae0f01591b208e654f81edd
ArturDumanskyi <dumanskyi1991@gmail.com>
2018-03-25 00:38:58 +0200

    Task_18

--- artur_hw_task_18.py
+++ artur_hw_task_18.py
@@ -2,10 +2,10 @@
 
 
 def sum_symbol_codes(first, last):
-  ord_1 = 0
+  total_sum = 0
   for i in range(ord(first), ord(last)+1):
-      ord_1 += i
-  return ord_1
+      total_sum += i
+  return total_sum
 
 
 print(sum_symbol_codes('x', 'y'))
